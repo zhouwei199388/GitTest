df0cff954507a6b9680cd4684715d66022e2f59b
zhouwei199388 <15090824065@163.com>
2016-08-31 16:35:39 +0800

    main创建

---
+++ app/src/main/java/androidtest/zw/com/gittest/MainActivity.java
@@ -0,0 +1,16 @@
+package androidtest.zw.com.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 2016/8/31.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
